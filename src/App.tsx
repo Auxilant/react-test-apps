5c0f8d2124d34f83e0ec125b454a13b92b67d5af
Vadim <clandestineblast@gmail.com>
2021-03-13 18:03:48 +0300

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from 'react-router-dom';
 import './App.scss';
+import Menu from './Menu'
 
 function App() {
-  return (
-    <></>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Menu />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/Menu.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Menu() {
+    return (
+        <div className="menu">
+            <h1>react-test-apps</h1>
+            <ul>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
