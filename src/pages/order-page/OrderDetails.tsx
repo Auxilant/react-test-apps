3353627ecb7b20c9bd0608212a90b2c80d46d3be
Vadim <clandestineblast@gmail.com>
2021-03-14 12:33:06 +0300

    useState, -/+ buttons

--- src/pages/order-page/OrderDetails.tsx
+++ src/pages/order-page/OrderDetails.tsx
@@ -1,23 +1,22 @@
-import { useReducer } from 'react'
+import { useState } from 'react'
 import { OrderData } from '../../interfaces/OrderData'
 import './OrderDetails.scss'
 import OrderItem from './OrderItem'
 
 function OrderDetails({orderData}: {orderData: OrderData}) {
-    const updater = (state: OrderData, {id, qty}:{id: number, qty: number}) => {
-        for (let item of state.items) {
-            if (item.id === id) {
-                item.qty = qty;
-                break;
-            }
-        }
-        console.log(state);
-        return state;
+    const [items, setItems] = useState(orderData.items);
+
+    const setItemQty = (id: number, qty: number) => {
+        const newItems = items.map(item => {
+            if (item.id === id) 
+                return {...item, qty};
+            else 
+                return item;
+        })
+        setItems(newItems);
     }
 
-    const [order, updateOrder] = useReducer(updater, orderData);
-
-    const total = order.items.reduce(
+    const total = items.reduce(
         (sum, item) => sum + item.qty * item.price, 0
     );
 
@@ -30,12 +29,11 @@ function OrderDetails({orderData}: {orderData: OrderData}) {
             </div>
             <h2>Товары:</h2>
             <div className="order-items">
-                {orderData.items.map (item => (
-                    <OrderItem key={item.id} item={item}/>
+                {items.map (item => (
+                    <OrderItem key={item.id} item={item} setItemQty={setItemQty}/>
                 ))}
             </div>
             <h3>Итого: {total} р.</h3>
-            <button onClick={() => updateOrder({id: 1, qty: 5})}>+</button>
         </div>
     )
 }
--- src/pages/order-page/OrderItem.tsx
+++ src/pages/order-page/OrderItem.tsx
@@ -1,12 +1,18 @@
 import { OrderItemData } from '../../interfaces/OrderData'
 
-function OrderItem({item}:{item: OrderItemData}) {
-    return(
-        <div className="order-item">
-            <span>{item.name} </span>
-            <span>{item.price} </span>
-            <span>{item.qty} </span>
-        </div>
+function OrderItem({ item, setItemQty }: { item: OrderItemData, setItemQty:(id: number, qty: number) => void }) {
+    return (
+        <div className="order-item-container">
+            <div className="order-item">
+                <span>{item.name} </span>
+                <span>{item.price} </span>
+                <button onClick={() => setItemQty(item.id, item.qty - 1)}>-</button>
+                <span>{item.qty} </span>
+                <button onClick={() => setItemQty(item.id, item.qty + 1)}>+</button>
+                <button>x</button>
+            </div>
+        </div >
+
     )
 }
 
